e2cf28362b21348ea3c4d17f69960720f32d12af
juanp77 <juanpaviles77@gmail.com>
2021-10-06 01:51:19 -0300

    Proceed to checkout flow added

--- cypress/pages/basePage.js
+++ cypress/pages/basePage.js
@@ -1,8 +1,27 @@
 /// <reference types ="Cypress" />
+import "cypress-real-events/support"
 
-export class BasePage{
-    goTo(){
+export class BasePage {
+    goTo() {
         cy.visit('http://automationpractice.com/index.php')
+    }
+
+    /**Top Menu Cart validation */
+    isProductInCart(prodTitle) {
+        //Simmulating mouseover
+        cy.get('[title="View my shopping cart"]').realHover() //trigger('mouseover')
+        cy.get('.cart_block').should('be.visible')
+        cy.get('.cart_block_product_name').should('contain.text', prodTitle)
+
+        cy.get('.products')
+    }
+
+    getTotal(totalPurchase) {
+        cy.get('.cart_block_total').should('have.text', totalPurchase)
+    }
+
+    clickOnCheckout() {
+        cy.get('#button_order_cart').click()
     }
 }
 
